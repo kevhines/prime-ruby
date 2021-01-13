1ed0c91fa082ebc34b9fd03545e5708d97758a98
Kevin <kevhines@gmail.com>
2021-01-12 22:39:35 -0500

    commit tree 6c9a7eac2170de55a94ef5cb64b842592638e1e6

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(test)
+    if test > 2
+        (2..test-1).to_a.none? do |divider|
+            test.remainder(divider) == 0
+        end  
+    elsif test == 2
+        true
+    else
+        false
+    end
+end
+
+
